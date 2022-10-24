ab025739e246277548c47591e0c37f514010d34b
lei <mia.wang90@gmail.com>
2022-10-24 17:13:33 +0900

    Add: exerciseモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :exercises
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    root to: "exercises#index"
+  end
+  resources :exercises
 end
