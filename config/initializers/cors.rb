4f1fb4c80614da048680c6ea70573ffbee334210
lei <mia.wang90@gmail.com>
2022-12-04 17:16:39 +0900

    Update front url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://daily-mind-2ao0swoso-lei900.vercel.app/"
+            "https://daily-mind-2ao0swoso-lei900.vercel.app"
 
     resource "*",
              headers: :any,
