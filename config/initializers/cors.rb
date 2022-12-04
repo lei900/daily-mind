67b80c1440c1214d6a03291286bc4f26773682cd
lei <mia.wang90@gmail.com>
2022-12-04 14:41:58 +0900

    Add cors url for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://127.0.0.1:3000"
+    origins "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://daily-mind-next-frontend.vercel.app/"
 
     resource "*",
              headers: :any,
