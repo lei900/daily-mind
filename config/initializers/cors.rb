238979883447ef80cced1f7ef1c096da909f39ba
lei <mia.wang90@gmail.com>
2022-12-06 13:09:50 +0900

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000",
-            "http://127.0.0.1:3000",
-            "https://daily-mind-lei900.vercel.app"
+    origins "*"
 
     resource "*",
              headers: :any,
