69d0a75582b95988bcc6e402ad31c58733c6fd12
lei <mia.wang90@gmail.com>
2022-12-11 17:29:44 +0900

    Fix choices_controller index action

--- app/controllers/api/v1/choices_controller.rb
+++ app/controllers/api/v1/choices_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::ChoicesController < Api::V1::BaseController
   skip_before_action :authenticate
 
   def index
-    choices = Choice.where(question_id: params[:qid])
+    question = Question.find_by(qid: params[:qid])
+    choices = question.choices
     json_string = ChoiceSerializer.new(choices).serializable_hash.to_json
     render json: json_string
   end
