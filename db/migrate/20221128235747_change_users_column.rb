960c9e3543b6cca71e6b1e40a316a807ffc4e626
lei <mia.wang90@gmail.com>
2022-12-15 16:46:56 +0900

    Recover user columns: avatar bio nickname

--- db/migrate/20221128235747_change_users_column.rb
+++ db/migrate/20221128235747_change_users_column.rb
@@ -2,10 +2,8 @@ class ChangeUsersColumn < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :sub, :uid
     add_index :users, :uid, unique: true
-    remove_column :users, :nickname, :string
-    remove_column :users, :bio, :string
     remove_column :users, :status, :integer
     remove_column :users, :username, :string
-    remove_column :users, :profile_picture, :string
+    rename_column :users, :profile_picture, :avatar
   end
 end
