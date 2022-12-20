c200a23e89273c53a69f0a6bf7d063f0cc37b4b3
lei <mia.wang90@gmail.com>
2022-12-20 18:08:06 +0900

    fix: add # rubocop:disable

--- app/controllers/api/v1/exercises/participations_controller.rb
+++ app/controllers/api/v1/exercises/participations_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Exercises::ParticipationsController < Api::V1::BaseController
 
   def update
     exercise = Exercise.find(params[:exercise_id])
-    exercise.increment!(:participants_number)
+    exercise.increment!(:participants_number) # rubocop:disable Rails/SkipsModelValidations
     head :ok
   end
 end
