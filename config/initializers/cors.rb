1292376d0b6021fb7630d65df15ec3ac8190a212
lei <mia.wang90@gmail.com>
2022-12-25 22:39:24 +0900

    fix cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "\Ahttps:\/\/daily-mind-\S*\.vercel\.app\/\S*"
+            "\Ahttps:\/\/daily-mind-\S*\.vercel\.app"
 
     resource "/api/v1/*",
              headers: :any,
