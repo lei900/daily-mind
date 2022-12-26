036b9a2a424679409002b93db1b93334581591f1
lei <mia.wang90@gmail.com>
2022-12-26 16:44:44 +0900

    recover entry_distortions

--- db/migrate/20221226060608_change_users_table.rb
+++ db/migrate/20221226060608_change_users_table.rb
@@ -1,8 +1,10 @@
 class ChangeUsersTable < ActiveRecord::Migration[7.0]
-  change_table :users do |t|
-    t.rename :sub, :uid
-    t.rename :profile_picture, :avatar
-    t.remove :status, :username
-    t.index :uid, unique: true
-  end
+  # def change
+  #   change_table :users do |t|
+  #     t.rename :sub, :uid
+  #     t.rename :profile_picture, :avatar
+  #     t.remove :status, :username
+  #     t.index :uid, unique: true
+  #   end
+  # end
 end
