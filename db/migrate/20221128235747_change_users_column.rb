22fca70bd6a1b3fa94e5f56aa800c857e7a37cf2
lei <mia.wang90@gmail.com>
2022-12-26 15:00:26 +0900

    remove change_users_column

--- db/migrate/20221128235747_change_users_column.rb
+++ db/migrate/20221128235747_change_users_column.rb
@@ -1,9 +1,9 @@
 class ChangeUsersColumn < ActiveRecord::Migration[7.0]
   def change
-    rename_column :users, :sub, :uid
-    add_index :users, :uid, unique: true
-    remove_column :users, :status, :integer
-    remove_column :users, :username, :string
-    # rename_column :users, :profile_picture, :avatar
+    # rename_column :users, :sub, :uid
+    # add_index :users, :uid, unique: true
+    # remove_column :users, :status, :integer
+    # remove_column :users, :username, :string
+    # # rename_column :users, :profile_picture, :avatar
   end
 end
