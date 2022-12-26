8cec3a13680574e43a70c3947a7c7982b7cb459f
lei <mia.wang90@gmail.com>
2022-12-26 16:55:40 +0900

    recover change_users_table

--- db/migrate/20221226060608_change_users_table.rb
+++ db/migrate/20221226060608_change_users_table.rb
@@ -1,10 +1,10 @@
 class ChangeUsersTable < ActiveRecord::Migration[7.0]
   def change
-    # change_table :users do |t|
-    #   t.rename :sub, :uid
-    #   t.rename :profile_picture, :avatar
-    #   t.remove :status, :username
-    #   t.index :uid, unique: true
-    # end
+    change_table :users do |t|
+      t.rename :sub, :uid
+      t.rename :profile_picture, :avatar
+      t.remove :status, :username
+      t.index :uid, unique: true
+    end
   end
 end
