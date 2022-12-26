f3623103ced43354b3f92a24a6a24d35810865be
lei <mia.wang90@gmail.com>
2022-12-26 15:08:44 +0900

    add change_users_table

---
+++ db/migrate/20221226060608_change_users_table.rb
@@ -0,0 +1,8 @@
+class ChangeUsersTable < ActiveRecord::Migration[7.0]
+  change_table :products do |t|
+    t.rename :sub, :uid
+    t.rename :profile_picture, :avatar
+    t.remove :status, :username
+    t.index :uid, unique: true
+  end
+end
