69a137f764a6887fbff2d53912c19ba63152a670
lei <mia.wang90@gmail.com>
2023-01-09 18:31:32 +0900

    add production domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://daily-mind-next-frontend.vercel.app"
+            ENV["DOMAIN"],
+            ENV["VERCEL_DOMAIN"]
 
     resource "/api/v1/*",
              headers: :any,
