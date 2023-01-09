d6a6dbb473b60d5b616f2152bcb068b2697f5197
lei <mia.wang90@gmail.com>
2023-01-09 18:38:32 +0900

    fix cors domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://daily-mind-next-frontend.vercel.app"
-
+            "https://www.idailymind.com",
+            "https://dailymind.vercel.app"
     resource "/api/v1/*",
              headers: :any,
              methods: %i[get post put patch delete options head]
